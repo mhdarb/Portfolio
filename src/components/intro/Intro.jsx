8973e84b34f549a86c47c2c037900da7b0061ccd
maj14 <mhdarb@gmail.com>
2021-10-17 10:30:03 +0530

    commit

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -12,7 +12,7 @@ export default function Intro() {
              backDelay: 1500,
              backSpeed: 60,
              showCursor:true,
-             strings: ['Muslim', 'Sunni', 'Sheikh' ],
+             strings: ['Muslim', 'Software Developer', 'Web Designer' ],
         });
     }, [])
     return (
@@ -24,7 +24,7 @@ export default function Intro() {
             </div>
             <div className="right">
                 <div className="wrapper">
-                    <h2>Hi There, I'm</h2>
+                    <h2>Assalamualiakum, I'm</h2>
                     <h1>Mohd Areeb</h1>
                     <h3><span ref={textRef}></span></h3>
                 </div>
