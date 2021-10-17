d3c0ffe4aa237eee6cf95e44b1c6330d8397c26a
maj14 <mhdarb@gmail.com>
2021-10-17 18:19:26 +0530

    commit

--- src/components/hobbies/Hobbies.jsx
+++ src/components/hobbies/Hobbies.jsx
@@ -1,9 +1,29 @@
-import './hobbies.scss'
-
+import { SportsCricket } from "@material-ui/icons";
+import './hobbies.scss';
 export default function Hobbies() {
     return (
-        <div>
-            
+        <div className="hobbies">
+            <div className="container">
+                <div className="hobby">
+                    <SportsCricket className="icon"/>
+                    <h3>Cricket</h3>
+                </div>
+                <div className="hobby">
+                    <h3>Cricket</h3>
+                </div>
+                <div className="hobby">
+                    <SportsCricket className="icon"/>
+                    <h3>Cricket</h3>
+                </div>
+                <div className="hobby">
+                    <SportsCricket className="icon"/>
+                    <h3>Cricket</h3>
+                </div>
+                <div className="hobby">
+                    <SportsCricket className="icon"/>
+                    <h3>Cricket</h3>
+                </div>
+            </div>
         </div>
     )
 }
