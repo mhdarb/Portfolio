4280bc913596e14c9bd9d9a34f6dcdcce70c81dd
maj14 <mhdarb@gmail.com>
2021-10-20 14:37:00 +0530

    commit

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,11 +1,36 @@
 import { init } from 'ityped';
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import profile_2 from "../../assets/2.jpeg";
+import profile_3 from "../../assets/3.jpg";
 import down from "../../assets/down.png";
-import profile from "../../assets/profile.png";
+import profile_1 from "../../assets/profile1.png";
 import "./intro.scss";
-
-
+const images = [profile_1, profile_2,profile_3];
 export default function Intro() {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            if(currentIndex === images.length - 1) {
+                setCurrentIndex(0);
+            } 
+            else {
+                setCurrentIndex(currentIndex + 1);
+            }
+        }, 3000)
+        
+        return () => clearInterval(intervalId);
+    }, [currentIndex])
+    // const [currentImage, setCurrentImage] = useState(null);
+
+    // useEffect(() => {
+    //     const intervalId = setInterval(() => {
+    //         setCurrentImage(images[Math.floor(Math.random() * images.length)]);
+    //     }, 1500)
+        
+    //     return () => clearInterval(intervalId);
+    // }, [])
+
     const textRef = useRef();
     useEffect(() => {
         init(textRef.current, {
@@ -19,7 +44,7 @@ export default function Intro() {
         <div className="intro" id="intro">
             <div className="left">
                 <div className="imgContainer">
-                    <img src={profile} alt="" />
+                    <img src={images[currentIndex]} alt="" />
                 </div>
             </div>
             <div className="right">
